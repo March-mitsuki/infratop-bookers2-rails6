67379edb7b6ec82e1153876af86504f5011126db
March-mitsuki <march_tsuki@icloud.com>
2022-10-02 03:21:34 +0900

    [change] ajax comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
     current_book = Book.find(params[:book_id])
-    comment = current_book.book_comments.new(comment_params)
-    comment.user_id = current_user.id
-    comment.save
-    redirect_back fallback_location: books_path
+    @comment = current_book.book_comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    # redirect_back fallback_location: books_path
   end
 
   def destroy
-    comment = BookComment.find(params[:id])
-    if comment.user.id == current_user.id
-      comment.destroy
-      redirect_back fallback_location: books_path
+    @comment = BookComment.find(params[:id])
+    if @comment.user.id == current_user.id
+      @comment.destroy
+      # redirect_back fallback_location: books_path
       return  
     else
       redirect_back fallback_location: books_path
